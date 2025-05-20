0ddc514adb9b30b5823c5e9947f3c41e221100ae
ventriloquo <dfern3321@gmail.com>
2025-05-20 01:03:33 -0300

    Updates

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("c46651ab216eb31e699be1bd5e6df8229b08005b534194c1ea92519b09661d71" "daa27dcbe26a280a9425ee90dc7458d85bd540482b93e9fa94d4f43327128077" "b1a691bb67bd8bd85b76998caf2386c9a7b2ac98a116534071364ed6489b695d" default))
  '(package-selected-packages
-   '(dracula-theme catppuccin-theme gruber-darker-theme all-the-icons telephone-line neotree god-mode simple-httpd company kanagawa-themes magit which-key gruvbox-theme htmlize)))
+   '(comment-tags bongo listen dracula-theme catppuccin-theme gruber-darker-theme all-the-icons telephone-line neotree god-mode simple-httpd company kanagawa-themes magit which-key gruvbox-theme htmlize)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
