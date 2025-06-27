8f088a6b51e3cfc7a2811c51617e8d6e60d9f05f
Ventriloquo <dfern3321@gmail.com>
2025-06-26 21:01:22 -0300

    Offline setup

--- init.el
+++ init.el
@@ -1,7 +1,11 @@
+;; Lists
+(add-to-list 'default-frame-alist '(font . "Iosevka-12"))
+(add-to-list 'load-path "~/.config/emacs/pkgs/yasnippet")
+
 ;; Requires
 (require 'ido)
 (require 'org-tempo)
-(require 'ansi-color)
+(require 'yasnippet)
 
 ;; Modes
 (icomplete-mode)
@@ -18,6 +22,10 @@
 (global-prettify-symbols-mode)
 (global-hl-line-mode t)
 (global-completion-preview-mode t)
+(yas-global-mode t)
+
+;; UTF-8 by default
+(prefer-coding-system 'utf-8)
 
 ;; Variables
 (setq backup-directory-alist '(("." . "~/.config/emacs/backups"))
@@ -41,49 +49,27 @@
 	      org-startup-with-inline-images t
 	      org-image-actual-width '(300))
 
-;; Lists
-(add-to-list 'default-frame-alist '(font . "Iosevka-12"))
-
 ;; Packages
-(load-file "~/.config/emacs/pkgs/gruber-darker-theme.el")
 (load-file "~/.config/emacs/pkgs/hare-mode.el")
+(load-file "~/.config/emacs/pkgs/gruber-darker.el")
+(load-file "~/.config/emacs/pkgs/hl-todo.el")
 
 ;; Theme
 (load-theme 'gruber-darker t)
 
 ;; Hooks
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
-(add-hook 'org-mode-hook #'visual-fill-column-mode)
-(add-hook 'compilation-filter-hook 'ansi-color-compilation-filter)
 
 ;; Keybindings
 (keymap-global-set "C-x C-b"		'ibuffer)
-(keymap-global-set "C-."		'ffap)
-(keymap-global-set "C-;"		'duplicate-line)
-(keymap-global-set "C-<tab> a"		'align-regexp)
 (keymap-global-set "C-<tab> e"		'eshell)
 (keymap-global-set "C-<tab> c"		'async-shell-command)
 (keymap-global-set "C-<tab> m c"	'compile)
 (keymap-global-set "C-<tab> m r"	'recompile)
+(keymap-global-set "C-."		'ffap)
+(keymap-global-set "C-;"		'duplicate-line)
 (keymap-global-set "C-<tab> r"		'restart-emacs)
 (keymap-global-set "C-<tab> <left>"	'windmove-left)
 (keymap-global-set "C-<tab> <right>"	'windmove-right)
 (keymap-global-set "C-<tab> <up>"	'windmove-up)
 (keymap-global-set "C-<tab> <down>"	'windmove-down)
-
-;; rmail
-(setq smtpmail-default-smtp-server "mail.cock.li")
-(require 'smtpmail)
-(setq rmail-inbox-list '("imaps://diogo%40cock.li:zJg0z%9q8t6.UPQX;U)9&#zXfKHYph$Ghna6E^P2$b!:[5c]ChV[7+1!Tdd7NP..@mail.cock.li")
-      read-mail-command	           'rmail
-      mail-user-agent	           'sendmail-user-agent
-      rmail-preserve-inbox         nil
-      rmail-mime-prefer-html       t
-      rmail-file-name              "~/.local/mail/inbox"
-      message-default-headers      "Fcc: ~/.local/mail/sent"
-      user-full-name               "Diogo"
-      user-mail-address            "diogo@cock.li"
-      shr-width                    80
-      send-mail-function           'smtpmail-send-it
-      smtpmail-stream-type         'ssl
-      smtpmail-smtp-service        465)
