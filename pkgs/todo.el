a8d2a6b3f698b1808502f9da5fe4772a40844d1f
Ventriloquo <dfern3321@gmail.com>
2025-07-03 20:52:06 -0300

    Simplificando as coisas

--- pkgs/todo.el
+++ pkgs/todo.el
@@ -10,21 +10,17 @@
   :lighter " TODO"
 
   (keymap-global-set "C-c t r"   'todo-highlight-mode-refresh)
-  (keymap-global-set "C-c t l t" 'todo-highlight-mode-list-todos)
-  (keymap-global-set "C-c t l w" 'todo-highlight-mode-list-warnings)
-  (keymap-global-set "C-c t l n" 'todo-highlight-mode-list-notes)
-  (keymap-global-set "C-c t l c" 'todo-highlight-mode-list-cautions)
-  (keymap-global-set "C-c t l f" 'todo-highlight-mode-list-fixmes)
+  (keymap-global-set "C-c t l" 'todo-highlight-mode-list-todos)
   
   (if todo-highlight-mode
       (todo-highlight-words)
     (remove-overlays)))
 
-(defvar WORD_NOTE	" ?NOTE: ?\\| ?note: ?\\| ?INFO: ?\\| ?info: ?")
-(defvar WORD_TODO	" ?TODO: ?\\| ?todo: ?\\| ?TBD: ?\\| ?tbd: ?")
-(defvar WORD_WARNING	" ?WARN: ?\\| ?WARNING: ?\\| ?warn: ?\\| ?warning: ?")
-(defvar WORD_CAUTION	" ?CAUTION: ?\\| ?caution: ?\\| ?BUG: ?\\| ?bug: ?")
-(defvar WORD_FIXME	" ?FIXME: ?\\| ?fixme: ?\\| ?FIX: ?\\| ?fix: ?")
+(defvar WORD_NOTE	" ?NOTE:.*\\| ?note:.*\\| ?INFO:.*\\| ?info:.*")
+(defvar WORD_TODO	" ?TODO:.*\\| ?todo:.*\\| ?TBD:.*\\| ?tbd:.*")
+(defvar WORD_WARNING	" ?WARN:.*\\| ?WARNING:.*\\| ?warn:.*\\| ?warning:.*")
+(defvar WORD_CAUTION	" ?CAUTION:.*\\| ?caution:.*\\| ?BUG:.*\\| ?bug:.*")
+(defvar WORD_FIXME	" ?FIXME:.*\\| ?fixme:.*\\| ?FIX:.*\\| ?fix:.*")
 
 (defun todo-highlight-mode-refresh ()
   (interactive)
@@ -40,28 +36,7 @@
 
 (defun todo-highlight-mode-list-todos ()
   (interactive)
-  (occur WORD_TODO)
-  (highlight-regexp WORD_TODO 'hi-green))
-
-(defun todo-highlight-mode-list-warnings ()
-  (interactive)
-  (occur WORD_WARNING)
-  (highlight-regexp WORD_WARNING 'hi-yellow))
-
-(defun todo-highlight-mode-list-notes ()
-  (interactive)
-  (occur WORD_NOTE)
-  (highlight-regexp WORD_NOTE 'hi-blue))
-
-(defun todo-highlight-mode-list-cautions ()
-  (interactive)
-  (occur WORD_CAUTION)
-  (highlight-regexp WORD_CAUTION 'hi-salmon))
-
-(defun todo-highlight-mode-list-fixmes ()
-  (interactive)
-  (occur WORD_FIXME)
-  (highlight-regexp WORD_FIXME 'isearch-fail))
+  (occur (concat WORD_NOTE WORD_TODO WORD_WARNING WORD_CAUTION WORD_FIXME)))
 
 (define-global-minor-mode global-todo-highlight-mode todo-highlight-mode
   (lambda ()
