59b3846748d1bcb3feff8b9206b845862f23e562
Ventriloquo <dfern3321@gmail.com>
2025-07-03 21:00:13 -0300

    Refining the config

--- init.el
+++ init.el
@@ -8,21 +8,21 @@
 (require 'yasnippet)
 
 ;; Modes
-(icomplete-mode)
-(ido-mode t)
-(ido-everywhere t)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tooltip-mode -1)
-(which-key-mode t)
-(delete-selection-mode t)
-(global-visual-line-mode t)
-(global-auto-revert-mode t)
-(global-prettify-symbols-mode)
-(global-hl-line-mode t)
-(global-completion-preview-mode t)
-(yas-global-mode t)
+(tool-bar-mode			-1)
+(menu-bar-mode			-1)
+(scroll-bar-mode		-1)
+(tooltip-mode			-1)
+(icomplete-mode			 t)
+(ido-mode			 t)
+(ido-everywhere			 t)
+(which-key-mode			 t)
+(delete-selection-mode		 t)
+(global-visual-line-mode	 t)
+(global-auto-revert-mode	 t)
+(global-prettify-symbols-mode	 t)
+(global-hl-line-mode		 t)
+(global-completion-preview-mode	 t)
+(yas-global-mode		 t)
 
 ;; UTF-8 by default
 (prefer-coding-system 'utf-8)
@@ -50,14 +50,14 @@
 	      org-image-actual-width '(300))
 
 ;; Keybindings
+(keymap-global-set "C-."		'ffap)
+(keymap-global-set "C-;"		'duplicate-line)
 (keymap-global-set "C-x C-b"		'ibuffer)
 (keymap-global-set "C-<tab> e"		'eshell)
 (keymap-global-set "C-<tab> c"		'async-shell-command)
+(keymap-global-set "C-<tab> r"		'restart-emacs)
 (keymap-global-set "C-<tab> m c"	'compile)
 (keymap-global-set "C-<tab> m r"	'recompile)
-(keymap-global-set "C-."		'ffap)
-(keymap-global-set "C-;"		'duplicate-line)
-(keymap-global-set "C-<tab> r"		'restart-emacs)
 (keymap-global-set "C-<tab> <left>"	'windmove-left)
 (keymap-global-set "C-<tab> <right>"	'windmove-right)
 (keymap-global-set "C-<tab> <up>"	'windmove-up)
@@ -67,11 +67,13 @@
 (load-file "~/.config/emacs/pkgs/simphare-mode.el")
 (load-file "~/.config/emacs/pkgs/simpc-mode.el")
 (load-file "~/.config/emacs/pkgs/gruber-darker-theme.el")
+(load-file "~/.config/emacs/pkgs/srcery-theme.el")
 
 ;; Theme
-(load-theme 'gruber-darker t)
+(load-theme 'srcery t)
 
 ;; Hooks
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
+(add-hook 'prog-mode-hook #'global-todo-highlight-mode)
 (add-hook 'c-mode-hook #'simpc-mode)
 
