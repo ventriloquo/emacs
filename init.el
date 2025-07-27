94f8b5bc844b1cca9156da3e89177089c5fdf0cd
Ventriloquo <dfern3321@gmail.com>
2025-07-26 21:49:41 -0300

    Updates

--- init.el
+++ init.el
@@ -1,17 +1,15 @@
 ;; Lists
-(add-to-list 'default-frame-alist '(font . "Iosevka-12"))
-(add-to-list 'load-path "~/.config/emacs/pkgs/yasnippet")
+(add-to-list 'default-frame-alist '(font . "Jetbrains Mono-11"))
 
 ;; Requires
 (require 'ido)
 (require 'org-tempo)
-(require 'yasnippet)
 
 ;; Modes
-(tool-bar-mode			-1)
-(menu-bar-mode			-1)
-(scroll-bar-mode		-1)
-(tooltip-mode			-1)
+(tool-bar-mode		        -1)
+(menu-bar-mode		        -1)
+(scroll-bar-mode	        -1)
+(tooltip-mode		        -1)
 (icomplete-mode			 t)
 (ido-mode			 t)
 (ido-everywhere			 t)
@@ -22,7 +20,6 @@
 (global-prettify-symbols-mode	 t)
 (global-hl-line-mode		 t)
 (global-completion-preview-mode	 t)
-(yas-global-mode		 t)
 
 ;; UTF-8 by default
 (prefer-coding-system 'utf-8)
@@ -32,28 +29,57 @@
       inhibit-startup-message t
       inhibit-startup-echo-area-message t
       tooltip-use-echo-area t
+      yas-snippet-dirs '("~/.config/emacs/snippets")
       global-auto-revert-non-file-buffers t
       use-dialog-box nil
       custom-file "~/.config/emacs/custom.el"
-      use-package-always-ensure t
       warning-minimum-level :error
-      minor-mode-alist nil
-      fill-column 80
-      org-edit-src-content-indentation 0
-      org-support-shift-select t
-      org-startup-indented t
-      org-pretty-entities t
-      org-use-sub-superscripts "{}"
-      org-hide-emphasis-markers t
-      org-startup-with-inline-images t
-      org-image-actual-width '(300))
+      dired-omit-files "\\.html\\|\\.aux\\|\\.pdf\\|\\.ini\\|\\.log\\|\\.toc\\|\\.fls\\|\\.out"
+      whitespace-style '(face
+			 tabs
+			 spaces
+			 space-mark  
+			 tab-mark))
+
+(setq-default minor-mode-alist nil
+	      fill-column 80
+	      which-key-idle-delay 0.1
+	      use-package-always-ensure t
+	      org-edit-src-content-indentation 0
+	      org-support-shift-select t
+	      org-startup-indented t
+	      org-pretty-entities t
+	      org-use-sub-superscripts "{}"
+	      org-hide-emphasis-markers t
+	      org-startup-with-inline-images t
+	      org-image-actual-width '(300))
+
+;; Loading files
+
+;;(load custom-file)
+
+(load-file "~/.config/emacs/themes/base16/base16-theme.el")
+(load-file "~/.config/emacs/pkgs/simphare-mode.el")
+(load-file "~/.config/emacs/pkgs/yasnippet.el")
+(load-file "~/.config/emacs/pkgs/todo.el")
+
+;; Theme
+(load-theme 'base16-everforest t)
+
+;; Hooks
+(add-hook 'prog-mode-hook	#'display-line-numbers-mode)
+(add-hook 'prog-mode-hook	#'global-todo-highlight-mode)
+(add-hook 'dired-mode-hook	#'dired-omit-mode)
+(add-hook 'after-init-hook	#'yas-global-mode)
 
 ;; Keybindings
+(keymap-global-set "C-x C-b"		'ibuffer)
 (keymap-global-set "C-."		'ffap)
 (keymap-global-set "C-;"		'duplicate-line)
-(keymap-global-set "C-x C-b"		'ibuffer)
 (keymap-global-set "C-<tab> e"		'eshell)
 (keymap-global-set "C-<tab> c"		'async-shell-command)
+(keymap-global-set "C-<tab> a"		'align-regexp)
+(keymap-global-set "C-<tab> o"		'dired-omit-mode)
 (keymap-global-set "C-<tab> r"		'restart-emacs)
 (keymap-global-set "C-<tab> m c"	'compile)
 (keymap-global-set "C-<tab> m r"	'recompile)
@@ -61,19 +87,4 @@
 (keymap-global-set "C-<tab> <right>"	'windmove-right)
 (keymap-global-set "C-<tab> <up>"	'windmove-up)
 (keymap-global-set "C-<tab> <down>"	'windmove-down)
-
-;; Packages
-(load-file "~/.config/emacs/pkgs/simphare-mode.el")
-(load-file "~/.config/emacs/pkgs/simpc-mode.el")
-(load-file "~/.config/emacs/pkgs/todo.el")
-
-;; Theme
-;;(load-file "~/.config/emacs/pkgs/gruber-darker-theme.el")
-(load-file "~/.config/emacs/pkgs/srcery-theme.el")
-(load-theme 'srcery t)
-
-;; Hooks
-(add-hook 'prog-mode-hook #'display-line-numbers-mode)
-(add-hook 'prog-mode-hook #'global-todo-highlight-mode)
-(add-hook 'c-mode-hook #'simpc-mode)
 
---
+++ themes/base16/base16-theme-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "base16-theme" "20250629.145"
+  "Collection of themes built on combinations of 16 base colors."
+  ()
+  :url "https://github.com/tinted-theming/base16-emacs"
+  :commit "244ae9a10f3bd4e9f09705da434989cd3831ddf6"
+  :revdesc "244ae9a10f3b"
+  :authors '(("Kaleb Elwert" . "belak@coded.io"))
+  :maintainers '(("Kaleb Elwert" . "belak@coded.io")))
